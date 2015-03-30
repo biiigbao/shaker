57b8e43a3468c4ce59e2e00462491e576a58e462
Mikolaj Kowalik <mxk62@psu.edu>
2015-03-30 09:24:49 -0400

    Fix function checking if there are overlaps
    Function isoverlap.m checking if a particle overlaps with its neighbors
    or any of the electrodes was calculating distances between the particle
    and electrodes incorrectly. I replaced erroneous equations with correct
    ones.

--- isoverlap.m
+++ isoverlap.m
@@ -1,24 +1,35 @@
 function flag = isoverlap(particle, neighbors, box)
 %ISOVERLAP Returns true value if there is an overlap.
-%   Detailed explanation goes here
+%   isoverlap(particle, neighbors, box) returns true if the particle
+%   overlaps with any of its neihbors or crosses any of electrodes.
+
+% Set minimal allowed distance between two neighboring spheres. Parameter
+% 'delta' 
+delta = 0.0;
+dmin = 2.0 + delta;
 
 rmol = particle.r;
-rmin = 2 + delta;
 
 % For now assume there is no overlaps.
 flag = 0;
 
-% Positions of image spheres.
-rbottom = [rmol(1); rmol(2); -rmol(3)];
-rtop = [rmol(1); rmol(2); 2 * H - rmol(3)];
-if norm(rbottom - rmol) < rmin || norm(rtop - rmol) < rmin
+% Check the distance between the particle and the closer electrode using
+% image approach, i.e. by calulating the distance between the particle and 
+% its "reflection" on the other side of an electrode.
+rimg = rmol;
+if rmol(3) > 0
+    rimg(3) = box(3) - rmol(3);
+else
+    rimg(3) = -box(3) - rmol(3);
+end
+if norm(rimg - rmol) < dmin
     flag = 1;
     return
 end
-    
+
+% Check if there are any overalaps with neighbors.
 for i = 1:length(neighbors)
-    neighbor = neigbors(i);
-    if dist(particle, neighbor, box) < rmin;
+    if dist(rmol, neighbors(i).r, box) < dmin;
         flag = 1;
         break
     end
